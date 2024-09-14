15230ad659b947372271d0ea7e0bfb606957699a
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-09-15 01:37:57 +0500

    create todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Todo {
+  id: number;
+  text: string;
 }
 
-export default App
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  function handleAdd() {
+    if (inputRef.current && inputRef.current.value.trim()) {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: inputRef.current.value,
+      };
+
+      const updatedTodos = [...todos, newTodo];
+      setTodos(updatedTodos);
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+      inputRef.current.value = ""; // input maydonini tozalash
+    }
+  }
+
+  return (
+    <div className="container mx-auto">
+      <div className="todos mt-40 border w-11/12 mx-auto shadow-md sm:w-4/5 md:w-1/2 p-3 md:p-8">
+        <div className="todos-header flex flex-row justify-between gap-5  rounded-2xl">
+          <input
+            ref={inputRef}
+            type="text"
+            className="w-full p-3 border"
+            placeholder="Writing todo..."
+          />
+          <button onClick={handleAdd} className="btn btn-success">
+            Add
+          </button>
+        </div>
+        <div className="todos-main flex flex-col gap-5 mt-5">
+          {todos.length > 0 ? (
+            todos.map((todo, index) => (
+              <div key={todo.id} className="todo border p-3">
+                <p>
+                  {index+1}. {todo.text}
+                </p>
+              
+              </div>
+            ))
+          ) : (
+            <p>No todos available</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
