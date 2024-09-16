ff829ef3f6ee977a66d78378b31cdc2b0750a4b2
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-09-16 08:27:29 +0500

    end homework

--- src/components/compiled/masala.js
+++ src/components/compiled/masala.js
@@ -1,56 +1,186 @@
 "use strict";
+// // 1
+// interface Person {
+//     name: string,
+//     age:number,
+//     isStudent: boolean
+// }
+// // 2
+// const student: Person = {
+//     name: "John Doe",
+//     age: 25,
+//     isStudent: false
+// }
+// // 3
+// interface Book {
+//     title: string,
+//     author: string,
+//     publishedYear: number
+// }
+// // 4
+// const bokk1: Book = {
+//     title: "The Great Gatsby",
+//     author: "F.Scott Fitzgerald",
+//     publishedYear: 1925
+// }
+// // -------------------------------------------------------
+// // 2 Massivlar
+// //  1
+// let sonlar: number[] = [1,  2, 3, 4, 5];
+// // 2
+// function sumArr(arr: number[]):number{
+//     let sum=0;
+//     arr.forEach(item=>{
+//         sum+=item
+//     })
+//     return sum
+// }
+// // console.log(sumArr(sonlar));
+// // 3
+// let str: string[] = ["apple", "banana", "cherry"];
+// // 4
+// function toUpperCase(txt: string[]):string[] {
+//     let newArr:string[] = [];
+//     txt.forEach(item=>{
+//         newArr.push(item.toUpperCase())
+//     })
+//     return newArr
+// }
+// // console.log(toUpperCase(str));
+// // ---------------------------------------------------===
+// // 3. **Tuple**
+// // 1
+// let StudentRecord: [string, number, boolean] = ['Alice', 22, true];
+// // 2
+// let Coordinate: [number, number] =[10, 20]
+// // -------------------------------------------------------
+// //**Mashq 4:** `any` Tipidan Foydalanish
+// let a: any = 'hello';
+// let b: any = 22;
+// let c: any = true;
+// // 2
+// let arr2: any = ["hello", 25, false, {name: 'John', age: 32}];
+// function outPut(arr: any):void{
+//     arr.forEach(element => {
+//        console.log(element);
+//     });
+// }
+// // outPut(arr2)
+// // ---------------------------------------------------
+// // **Mashq 5:** `unknown` Tipidan Foydalanish
+// let d: unknown;
+// // 1
+// d="hello";
+// if(typeof d === "string"){
+//     console.log('string');
+// }
+// // 2
+// let e:unknown;
+// e=true;
+// if(typeof e === "string"){
+//     console.log(e);
+// }else if(typeof e !== "string"){
+//     console.log('String emas');
+// }
+// **Mashq 6:** `never` Tipidan Foydalanish
+// 1
+function errr(txt) {
+    throw new Error(txt);
+}
+// errr();
 // 2
-const student = {
-    name: "John Doe",
-    age: 25,
-    isStudent: false
-};
-// 4
-const bokk1 = {
-    title: "The Great Gatsby",
-    author: "F.Scott Fitzgerald",
-    publishedYear: 1925
-};
-// -------------------------------------------------------
-// 2 Massivlar
-//  1
-let sonlar = [1, 2, 3, 4, 5];
+function errorFunction() {
+    while (true) {
+        console.log("Error");
+    }
+}
+// errorFunction()
+// **Mashq 7:** Funksiyalarni Yozish
+// 1
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+// console.log(addNumbers(1,2));
 // 2
-function sumArr(arr) {
+function formatDate(txt) {
+    let arr = txt.split(".");
+    let result = `${arr[2]}-${arr[1]}-${arr[0]}`;
+    return result;
+}
+// console.log(formatDate("16.09.2024"));
+// **Mashq 8:** `Enum`dan Foydalanish
+// 1
+var DaysOfWeek;
+(function (DaysOfWeek) {
+    DaysOfWeek[DaysOfWeek["Monday"] = 0] = "Monday";
+    DaysOfWeek[DaysOfWeek["Tuesday"] = 1] = "Tuesday";
+    DaysOfWeek[DaysOfWeek["Wednesday"] = 2] = "Wednesday";
+    DaysOfWeek[DaysOfWeek["Thursday"] = 3] = "Thursday";
+    DaysOfWeek[DaysOfWeek["Friday"] = 4] = "Friday";
+    DaysOfWeek[DaysOfWeek["Saturday"] = 5] = "Saturday";
+    DaysOfWeek[DaysOfWeek["Sunday"] = 6] = "Sunday";
+})(DaysOfWeek || (DaysOfWeek = {}));
+// 2
+function printDay(a) {
+    return a;
+}
+let person1 = {
+    name: "John",
+    age: 35,
+    employeeId: 14,
+    department: "Engineer"
+};
+// 3
+let car1 = {
+    make: "CHEVROLET",
+    model: "Malibu 2",
+    year: 2024,
+    numberOfDoors: 4,
+    isElectric: false
+};
+// 3
+let person3 = {
+    name: "Mak",
+    age: 28,
+    isStudent: true,
+    address: {
+        street: "Main St",
+        city: "Springfield",
+        postalCode: "12345"
+    }
+};
+let student1 = {
+    name: "John",
+    age: 28,
+    grades: [4, 5, 3, 4, 5],
+    address: ["Main St", 15]
+};
+function avarageGrades(arr) {
     let sum = 0;
     arr.forEach(item => {
         sum += item;
     });
-    return sum;
+    return sum / arr.length;
 }
-// console.log(sumArr(sonlar));
-// 3
-let str = ["apple", "banana", "cherry"];
-// 4
-function toUpperCase(txt) {
-    let newArr = [];
-    txt.forEach(item => {
-        newArr.push(item.toUpperCase());
-    });
-    return newArr;
+//2 
+function getStudentSummary(student) {
+    return `${student.name} ${student.age} ${avarageGrades(student.grades)}`;
 }
-// console.log(toUpperCase(str));
-// ---------------------------------------------------===
-// 3. **Tuple**
+// console.log(getStudentSummary(student1));
+// **Mashq 13:** Enum, Type Alias va Intersectionni Aralashtirib Ishlash
 // 1
-let StudentRecord = ['Alice', 22, true];
-// 2
-let Coordinate = [10, 20];
-// -------------------------------------------------------
-//**Mashq 4:** `any` Tipidan Foydalanish
-let a = 'hello';
-let b = 22;
-let c = true;
-// 2
-let arr2 = ["hello", 25, false, { name: 'John', age: 32 }];
-function outPut(arr) {
-    arr.forEach(element => {
-        console.log(element);
-    });
-}
-// outPut(arr2)
+var orderStatus;
+(function (orderStatus) {
+    orderStatus[orderStatus["Pending"] = 0] = "Pending";
+    orderStatus[orderStatus["Shipped"] = 1] = "Shipped";
+    orderStatus[orderStatus["Delivered"] = 2] = "Delivered";
+    orderStatus[orderStatus["Cancelled"] = 3] = "Cancelled";
+})(orderStatus || (orderStatus = {}));
+// 5
+let customer1 = {
+    orderId: 456,
+    status: orderStatus.Shipped,
+    totalAmount: 150.00,
+    customerID: 789,
+    name: "Sarah"
+};
--- src/components/masala.tsx
+++ src/components/masala.tsx
@@ -1,86 +1,296 @@
+// // 1
+// interface Person {
+//     name: string,
+//     age:number,
+//     isStudent: boolean
+// }
+
+// // 2
+// const student: Person = {
+//     name: "John Doe",
+//     age: 25,
+//     isStudent: false
+// }
+
+// // 3
+// interface Book {
+//     title: string,
+//     author: string,
+//     publishedYear: number
+// }
+
+// // 4
+
+// const bokk1: Book = {
+//     title: "The Great Gatsby",
+//     author: "F.Scott Fitzgerald",
+//     publishedYear: 1925
+// }
+
+// // -------------------------------------------------------
+
+// // 2 Massivlar
+// //  1
+// let sonlar: number[] = [1,  2, 3, 4, 5];
+
+// // 2
+// function sumArr(arr: number[]):number{
+//     let sum=0;
+//     arr.forEach(item=>{
+//         sum+=item
+//     })
+
+//     return sum
+// }
+// // console.log(sumArr(sonlar));
+
+// // 3
+// let str: string[] = ["apple", "banana", "cherry"];
+
+// // 4
+// function toUpperCase(txt: string[]):string[] {
+//     let newArr:string[] = [];
+//     txt.forEach(item=>{
+//         newArr.push(item.toUpperCase())
+//     })
+//     return newArr
+// }
+// // console.log(toUpperCase(str));
+
+// // ---------------------------------------------------===
+// // 3. **Tuple**
+
+// // 1
+// let StudentRecord: [string, number, boolean] = ['Alice', 22, true];
+
+// // 2
+// let Coordinate: [number, number] =[10, 20]
+
+// // -------------------------------------------------------
+
+// //**Mashq 4:** `any` Tipidan Foydalanish
+
+// let a: any = 'hello';
+// let b: any = 22;
+// let c: any = true;
+
+// // 2
+// let arr2: any = ["hello", 25, false, {name: 'John', age: 32}];
+
+// function outPut(arr: any):void{
+//     arr.forEach(element => {
+//        console.log(element);
+//     });
+// }
+// // outPut(arr2)
+
+// // ---------------------------------------------------
+
+// // **Mashq 5:** `unknown` Tipidan Foydalanish
+
+// let d: unknown;
+// // 1
+// d="hello";
+// if(typeof d === "string"){
+//     console.log('string');
+// }
+
+// // 2
+// let e:unknown;
+// e=true;
+// if(typeof e === "string"){
+//     console.log(e);
+// }else if(typeof e !== "string"){
+//     console.log('String emas');
+// }
+
+// **Mashq 6:** `never` Tipidan Foydalanish
+
 // 1
-interface Person {
-    name: string,
-    age:number,
-    isStudent: boolean
+function errr(txt: string):never{
+    throw new Error(txt)
+}
+// errr();
+
+// 2
+function errorFunction(): never{
+    while(true){
+        console.log("Error");
+    }
+}
+// errorFunction()
+
+// **Mashq 7:** Funksiyalarni Yozish
+
+// 1
+function addNumbers(num1:number, num2:number): number{
+    return num1+num2;
+}
+// console.log(addNumbers(1,2));
+
+// 2
+function formatDate(txt:string): string{
+    let arr=txt.split(".");
+    let result=`${arr[2]}-${arr[1]}-${arr[0]}`
+    return result
+}
+// console.log(formatDate("16.09.2024"));
+
+// **Mashq 8:** `Enum`dan Foydalanish
+
+// 1
+enum DaysOfWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
 }
 
 // 2
-const student: Person = {
-    name: "John Doe",
-    age: 25,
-    isStudent: false
+function printDay(a: any): number{
+    return a
+}
+// console.log(printDay(DaysOfWeek.Friday));
+
+// **Mashq 9:** Intersection Tiplari
+
+// 1
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface Employee {
+    employeeId: number;
+    department: string;
+}
+
+type Worker1 = Person & Employee;
+
+let person1: Worker1 = {
+    name: "John",
+    age: 35,
+    employeeId: 14,
+    department: "Engineer" 
+};
+// console.log(person1);
+
+// **Mashq 10:** Interfeyslar
+// 1
+interface Vehicle {
+    make: string,
+    model: string,
+    year: number
+}
+
+// 2
+interface Car extends Vehicle {
+    numberOfDoors: number,
+    isElectric: boolean
 }
 
 // 3
-interface Book {
-    title: string,
-    author: string,
-    publishedYear: number
+let car1: Car = {
+    make: "CHEVROLET",
+    model: "Malibu 2",
+    year: 2024,
+    numberOfDoors: 4,
+    isElectric: false
 }
 
-// 4
+// **Mashq 11:** Type Alias
 
-const bokk1: Book = {
-    title: "The Great Gatsby",
-    author: "F.Scott Fitzgerald",
-    publishedYear: 1925
+type Address = {
+    street: string,
+    city: string,
+    postalCode: string
 }
 
-// -------------------------------------------------------
-
-// 2 Massivlar
-//  1
-let sonlar: number[] = [1,  2, 3, 4, 5];
+interface Person {
+    name: string,
+    age: number,
+    isStudent: boolean,
+}
 
 // 2
-function sumArr(arr: number[]):number{
+type PersonWithAddress = Person & {address: Address}
+
+// 3
+let person3: PersonWithAddress = {
+    name: "Mak",
+    age: 28,
+    isStudent: true,
+    address: {
+        street: "Main St",
+        city: "Springfield",
+        postalCode: "12345"
+    }
+}
+// console.log(person3);
+
+// **Mashq 12:** Obyekt, Funksiya va Tupleni Aralashtirib Ishlash
+// 1
+interface Student {
+    name: string,
+    age: number,
+    grades: number[],
+    address: [string, number]
+}
+let student1: Student = {
+    name: "John",
+    age: 28,
+    grades: [4,5,3,4,5],
+    address: ["Main St", 15]
+}
+
+function avarageGrades(arr: []): number{
     let sum=0;
     arr.forEach(item=>{
         sum+=item
     })
-
-    return sum
+    return sum/arr.length
 }
-// console.log(sumArr(sonlar));
 
-// 3
-let str: string[] = ["apple", "banana", "cherry"];
-
-// 4
-function toUpperCase(txt: string[]):string[] {
-    let newArr:string[] = [];
-    txt.forEach(item=>{
-        newArr.push(item.toUpperCase())
-    })
-    return newArr
+//2 
+function getStudentSummary(student: any):string{
+    return `${student.name} ${student.age} ${avarageGrades(student.grades)}`
 }
-// console.log(toUpperCase(str));
+// console.log(getStudentSummary(student1));
 
-// ---------------------------------------------------===
-// 3. **Tuple**
+// **Mashq 13:** Enum, Type Alias va Intersectionni Aralashtirib Ishlash
 
 // 1
-let StudentRecord: [string, number, boolean] = ['Alice', 22, true];
-
-// 2
-let Coordinate: [number, number] =[10, 20]
-
-// -------------------------------------------------------
-
-//**Mashq 4:** `any` Tipidan Foydalanish
-
-let a: any = 'hello';
-let b: any = 22;
-let c: any = true;
-
-// 2
-let arr2: any = ["hello", 25, false, {name: 'John', age: 32}];
-
-function outPut(arr: any):void{
-    arr.forEach(element => {
-       console.log(element);
-    });
+enum orderStatus {
+    Pending,
+    Shipped,
+    Delivered,
+    Cancelled
 }
-// outPut(arr2)
 
+// 2
+interface Order {
+    orderId: number,
+    status: orderStatus,
+    totalAmount: number
+}
+
+// 3
+interface Customer {
+    customerID: number,
+    name: string
+}
+
+// 4
+type CustomerOrder = Order & Customer
+
+// 5
+let customer1: CustomerOrder = {
+    orderId: 456,
+    status: orderStatus.Shipped,
+    totalAmount: 150.00,
+    customerID: 789,
+    name: "Sarah"
+}
